c2cfcf28a452b7b02ec1f5c4fbc809f52a011457
tony <planet@hongxin.eu.org>
2023-10-17 12:02:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ export NEZHA_SERVER="data.hongxin.eu.org:443"
 export NEZHA_KEY="iXmbYD5ymoQA3L6i4Q"
 
 chmod +x server start.sh
-nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #！若需要tls，在此句 > 前面加上--tls即可
+nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} --tls > /dev/null 2>&1 &   #！若需要tls，在此句 > 前面加上--tls即可
 
 tail -f /dev/null
+ 
